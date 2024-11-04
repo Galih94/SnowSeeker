06410543c7774142c254d4fc2bf8ff3ece214aab
galih <samudragalih94@gmail.com>
2024-11-04 09:27:56 +0700

    wip initial project

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Galih Samudra on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
