dee75550ef2bc052727183c19ff0ec030a67c599
galih <samudragalih94@gmail.com>
2024-11-05 11:07:48 +0700

    clean code

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,26 +7,9 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Galih")
-            Text("Country: Indonesia")
-            Text("Pets: Gecko")
-        }
-        .font(.title)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     var body: some View {
-        ViewThatFits {
-            Rectangle()
-                .frame(width: 500, height: 200)
-            Circle()
-                .frame(width: 200, height: 200)
-        }
+        Text("Hello world")
     }
 }
 
