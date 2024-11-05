3f3a8d389011af409d75ae886875084dbd7c77fb
galih <samudragalih94@gmail.com>
2024-11-05 10:10:07 +0700

    feat(sheet): Use an optional Identifiable object as the condition for showing the sheet. As soon as the sheet is dismissed, SwiftUI sets selectedUser back to nil.

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    
+    @State private var selectedUser: User? = nil
+    
     var body: some View {
-        Text("Hello world")
+        Button("Tap Me") {
+            selectedUser = User()
+        }
+        .sheet(item: $selectedUser) { user in
+            Text(user.id)
+        }
     }
 }
 
