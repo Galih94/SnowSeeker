aa7264ff78d6b1c06daab9dca7a764aa97c9ffc8
galih <samudragalih94@gmail.com>
2024-11-05 09:47:18 +0700

    clean code

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,16 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView(preferredCompactColumn: .constant(.detail)) {
-            NavigationLink("Primary") {
-                Text("New view")
-            }
-        } detail: {
-            Text("Content")
-                .navigationTitle("Content View")
-                .toolbar(.hidden, for: .navigationBar)
-        }
-        .navigationSplitViewStyle(.balanced)
+        Text("Hello world")
     }
 }
 
