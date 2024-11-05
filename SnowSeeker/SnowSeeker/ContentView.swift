95855649005cedc9251de5eca9588e8ea4cd3df5
galih <samudragalih94@gmail.com>
2024-11-05 10:27:07 +0700

    clean code

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,27 +7,9 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
-}
-
 struct ContentView: View {
-    
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
-    
     var body: some View {
-        Button("Tap Me") {
-            selectedUser = User()
-            isShowingUser = true
-        }
-        .sheet(item: $selectedUser) { user in
-            Text(user.id)
-                .presentationDetents([.medium, .large])
-        }
-//        .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
-//            Button(user.id) {}
-//        }
+        Text("Hello world")
     }
 }
 
