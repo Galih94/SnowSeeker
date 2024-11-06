ee34e92fe015ff10afacdc0a5f572496a92836ce
galih <samudragalih94@gmail.com>
2024-11-06 11:19:33 +0700

    feat(ResortView): Create a new SwiftUI view called ResortView to show Resort detail UI

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -34,6 +34,9 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Resorts")
+            .navigationDestination(for: Resort.self) { resort in
+                ResortView(resort: resort)
+            }
         } detail: {
             WelcomeView()
         }
